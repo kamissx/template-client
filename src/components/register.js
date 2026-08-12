a26bcf5b50ebf9f3c67831465e740d83bd837f04
kamissx <kamilly_saraiva@estudante.sesisenai.org.br>
2026-08-12 20:32:16 -0300

    Update register.js

--- src/components/register.js
+++ src/components/register.js
@@ -26,7 +26,7 @@ function Registrar() {
     const manipularBotao = async () => {
         try {
             const response = await
-                axios.post('http://localhost:5000/registrar', {
+                axios.post('https://template-server-1.onrender.com/registrar', {
                     nome: nome,
                     email: email,
                     senha: senha,
